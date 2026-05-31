9b8eb160df0b2fd8a404244f7da3808a1f5a17fb
EricMwangi <ericmwas01@gmail.com>
2026-05-31 13:33:27 +0300

    Added clang format GNU
    Signed-off-by: EricMwangi <ericmwas01@gmail.com>

--- vmm/core /Initialization/type_definitions.h
+++ vmm/core /Initialization/type_definitions.h
@@ -1,40 +1,38 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <linux/errno.h>   
-#include <linux/kvm.h>
 #include <asm/kvm.h>
+#include <fcntl.h>
+#include <linux/errno.h>
+#include <linux/kvm.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
-
-struct 
-spartan_vm_description 
+struct spartan_vm_description
 {
-    size_t kvm_fd;
-    int KVM_VERSION; 
-    struct spartan_errors* 
+  size_t kvm_fd;
+  int KVM_VERSION;
+  struct spartan_errors *
 
-} spartan_vm_description; 
-// 
+} spartan_vm_description;
+//
 
 // mut
-int spartan_update_vm_description()
+int
+spartan_update_vm_description ()
 {
-    //TODO: implement a mutex controlled update of the vm description 
-    return 1; 
+  // TODO: implement a mutex controlled update of the vm description
+  return 1;
 }
-struct 
-spartan_errors
+struct spartan_errors
 {
-    // linux errors reach 133
-    // the application starts with higher error numbers 
-    // to avoid conflict 
-    uint8_t VERSION_ERROR;  
-} spartan_errors; 
+  // linux errors reach 133
+  // the application starts with higher error numbers
+  // to avoid conflict
+  uint8_t VERSION_ERROR;
+} spartan_errors;
 
-struct spartan_errors*
-spartan_errors_intialize(struct spartan_vm_description* vm_desc)
+struct spartan_errors *
+spartan_errors_intialize (struct spartan_vm_description *vm_desc)
 {
-    struct spartan_errors* alloc =(struct spartan_errors*)malloc(sizeof(spartan_errors)); 
-
+  struct spartan_errors *alloc
+      = (struct spartan_errors *)malloc (sizeof (spartan_errors));
 }
